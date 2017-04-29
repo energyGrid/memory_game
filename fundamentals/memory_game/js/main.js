8baf8905943c48e4975d128512ea1e5fb79eac20
solarvswind <scottpiercew@gmail.com>
2017-04-29 16:26:55 -0400

    version after unit 9

--- fundamentals/memory_game/js/main.js
+++ fundamentals/memory_game/js/main.js
@@ -1,14 +1,26 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push("cardOne");
-console.log("User flipped queen");
-var cardTwo = cards[2];
-cardsInPlay.push("cardTwo");
-console.log("User flipped king");
-//if cardsInPlay has 2 cards
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You found a match!");
-} else if (cardsInPlay[0] !== cardsInPlay[1]) {
-	alert("Sorry, try again.");
-}
+
+var checkForMatch = function() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+      console.log("You found a match!");
+  } else {
+      console.log("Sorry, try again.");
+  }
+};
+
+var flipCard = function(cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	//if cardsInPlay array is 2 and the cards are the same 
+		//send alert
+	if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+	checkForMatch()
+	};
+
+flipCard(0);
+flipCard(2);

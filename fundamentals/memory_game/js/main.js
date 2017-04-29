288a8c01e344b68f1a4ce5d551a27d42d0cd404f
Scott Pierce <scottpiercew@gmail.com>
2017-04-29 10:34:58 -0400

    fundamentals folder added

---
+++ fundamentals/memory_game/js/main.js
@@ -0,0 +1,14 @@
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push("cardOne");
+console.log("User flipped queen");
+var cardTwo = cards[2];
+cardsInPlay.push("cardTwo");
+console.log("User flipped king");
+//if cardsInPlay has 2 cards
+if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!");
+} else if (cardsInPlay[0] !== cardsInPlay[1]) {
+	alert("Sorry, try again.");
+}
